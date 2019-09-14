16f6c29e94b208f4621b8d4d94a1156568a0bb1b
Kiangkai62 <52350403+Kiangkai62@users.noreply.github.com>
2019-09-14 19:30:51 +0700

    Add files via upload

---
+++ Test (2).py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #1
+
+# In[17]:
+
+
+A = 2562
+B = int(input("ปีเกิด, "))
+C = A-B
+print (C)
+
+
+# # 2
+
+# In[23]:
+
+
+D = int(input(ปลาเค็ม"1*20, "))
+A = int(input(ไก่ทอด"1*45, "))
+B = int(input(ปลาทอด"1*34, "))
+print (D,A,B)
+
+
+# # 3
+
+# In[16]:
+
+
+for k in range(1,12) :
+    print(k)
+    k+=1
+print(k)
+
+
+# # 4
+
+# In[15]:
+
+
+k = Hello
+g = World
+for k in range(5) :
+    print(k)
+    k+=1
+print(k)
+
+
+# In[ ]:
+
+
+
+
